4ec5b003d712a28e1a5feaaa236d0d16455ff6c3
Diego Rivas <rivasdiego.dev@gmail.com>
2024-11-14 02:12:27 -0600

    Wrap Logotype in a Link component and update Header sidebar with Login and Register links

--- src/components/atoms/Logotype/Logotype.tsx
+++ src/components/atoms/Logotype/Logotype.tsx
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Logotype() {
     return (
-        <div className="flex items-center">
-            <span className="text-white text-3xl font-bold">Bike</span>
-            <span className="bg-[#ff9000] text-black text-3xl font-bold px-1 rounded-sm ml-1">hub</span>
-        </div>
+        <Link to={'/'}>
+            <div className="flex items-center">
+                <span className="text-white text-3xl font-bold">Bike</span>
+                <span className="bg-[#ff9000] text-black text-3xl font-bold px-1 rounded-sm ml-1">hub</span>
+            </div>
+        </Link>
     )
 }
--- src/components/organisms/Header/Header.tsx
+++ src/components/organisms/Header/Header.tsx
@@ -24,11 +24,11 @@ export default function Header() {
                     </div>
                 </div>
             </div>
-            <div className="drawer-side">
+            <div className="drawer-side z-10">
                 <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
                 <ul className="menu bg-base-200 min-h-full w-80 p-4">
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
+                    <li><Link to={'login'}> Login </Link></li>
+                    <li><Link to={'register'} > Register </Link></li>
                 </ul>
             </div>
         </div>
